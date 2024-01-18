d39f9d7ce6a83514b477980ef79a26bdb14ba0ae
izm-ka <izm.karina@gmail.com>
2024-01-18 14:42:21 +0700

    system(command) error

--- 22213/k.izmailova/lab8/lab8.c
+++ 22213/k.izmailova/lab8/lab8.c
@@ -1,4 +1,3 @@
-                                                     
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -8,44 +7,45 @@
 
 int main(int argc, char* argv[])
 {
-        struct flock lock;
-        int fd;
-        char command[FILENAME_MAX];
+    struct flock lock;
+    int fd;
+    char command[FILENAME_MAX];
 
-        fd = open(argv[1], O_RDWR);
-        if (fd == -1)
+    fd = open(argv[1], O_RDWR);
+    if (fd == -1)
+    {
+        perror("Can't open file");
+        return 1;
+    }
+
+    lock.l_type = F_WRLCK;
+    lock.l_whence = SEEK_SET;
+    lock.l_start = 0; //
+    lock.l_len = 0; //
+
+    if (fcntl(fd, F_SETLK, &lock) == -1)
+    {
+        if ((errno == EACCES) || (errno == EAGAIN))
         {
-                perror("Can't open file");
-                return 1;
+            printf("Try arain later, file is busy\n");
+            close(fd);
+            return 0;
         }
+        perror("Can't lock file");
+        close(fd);
+        return 1;
+    }
 
-        lock.l_type = F_WRLCK;
-        lock.l_whence = SEEK_SET;
-        lock.l_start = 0;
-        lock.l_len = 0;
+    sprintf(command, "vim %s\n", argv[1]);
 
-        if (fcntl(fd, F_SETLK, &lock) == -1)
-        {
-                if ((errno == EACCES) || (errno == EAGAIN))
-                {
-                        printf("Try arain later, file is busy\n");
-                        close(fd);
-                        return 0;
-                }
-                perror("Can't lock file");
-                close(fd);
-                return 1;
-        }
-
-        sprintf(command, "vim %s\n", argv[1]);
-        system(command);
+    if (system(command) == -1) {
         lock.l_type = F_UNLCK;
-        if (fcntl(fd, F_SETLK, &lock) == -1)
-        {
-                perror("Can't unlock file");
-                close(fd);
-                return 1;
+        if (fcntl(fd, F_SETLK, &lock) == -1) {
+            perror("Can't unlock file");
         }
         close(fd);
-        return 0;
+        return 1;
+    }
+    return 0;
+    //рассказать про блокировки. виды и взаимодействия 
 }
